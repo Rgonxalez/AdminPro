4a4aab2aa38a39444708252ea42661822f82d615
RenzoGCh <r.gonxalezch@gmail.com>
2019-11-01 21:53:28 -0500

    Actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//RUTAS
+import {APP_ROUTES} from './app.routes';
+//Importamos las Rutas
+import {PagesModule} from './pages/pages.module';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+   
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
